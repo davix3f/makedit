82d323b3406ef2f9333c4d221de3c188cce51738
Davide <davide_fiorito@libero.it>
2018-07-09 17:44:53 +0200

    Initial files + dev branch

---
+++ button_functions.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <gtkmm.h>
+
+void set_mainwindow(Gtk::Window *mainwindow);
+void set_textview(Gtk::TextView *textview);
+
+void openfile_click();
+
+void save_click();
+
+void saveas_click();
+
+void newfile_click();
+
+void make_click();
+
+void execmd();
