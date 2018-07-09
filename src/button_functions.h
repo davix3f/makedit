9ba8459f9a3851373bdf6b540220f9f3e5526943
Davide <davide_fiorito@libero.it>
2018-07-10 01:05:09 +0200

    Added function naming

--- src/button_functions.h
+++ src/button_functions.h
@@ -3,15 +3,11 @@
 
 void set_mainwindow(Gtk::Window *mainwindow);
 void set_textview(Gtk::TextView *textview);
+void set_builder(Glib::RefPtr<Gtk::Builder> mainbuilder);
 
 void openfile_click();
-
 void save_click();
-
 void saveas_click();
-
 void newfile_click();
-
 void make_click();
-
 void execmd();
