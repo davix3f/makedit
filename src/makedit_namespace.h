d4f7d7bcb6e19e9e53f547ac3d6adb1c24105079
Davide <davide_fiorito@libero.it>
2018-07-11 03:03:52 +0200

    Main parent; question_dialog
    Use question_dialog to quickly summon a question dialog with OK and CANCEL as answers

--- src/makedit_namespace.h
+++ src/makedit_namespace.h
@@ -2,6 +2,12 @@
 #include <gtkmm.h>
 
 namespace supportive
-{
+{	
+	extern Gtk::Window *main_parent;
+	void set_mainwindow(Gtk::Window *mainwindow);
+	
 	extern Glib::RefPtr<Gtk::Builder> glade_builder;
+
+	extern Gtk::Dialog *alertdialog;
+	int question_dialog(const char* message, const char* additional_message);
 }
