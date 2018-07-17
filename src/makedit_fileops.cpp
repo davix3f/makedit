c08fb620dddf0306bbfc35e271037f951de3395b
Davide <davide_fiorito@libero.it>
2018-07-18 01:47:24 +0200

    Operations on file supporters

---
+++ src/makedit_fileops.cpp
@@ -0,0 +1,23 @@
+#include "makedit_namespaces.h"
+
+
+
+namespace makedit_fileops
+{
+	int file_write_permission_checker(const char* file_name)
+	{
+		std::fstream file;
+		file.open(file_name, std::ios::app);
+		if(file.is_open())
+		{
+			std::cout << "File \'" << file_name << "\' can be opened in READ-WRITE mode" << std::endl;
+			file.close();
+			return(true);
+		}
+		else
+		{
+			std::cout << "File \'" << file_name << "\' can be opened in READ-ONLY mode, or doesn't exist" << std::endl;
+			return(false);
+		}
+	}
+}
