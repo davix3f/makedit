4bf7db776667d56fa9430ebe5a0a5225f8ff4d27
Davide <davide_fiorito@libero.it>
2018-07-18 01:47:45 +0200

    Import corrected

--- src/openfile.h
+++ src/openfile.h
@@ -2,6 +2,6 @@
 #include <fstream>
 #include <string>
 #include <gtkmm.h>
-#include "makedit_namespace.h"
+#include "makedit_namespaces.h"
 
 void openfile_action(Gtk::Window *parent, Gtk::TextView *textview, Gtk::HeaderBar *bottom_bar);
